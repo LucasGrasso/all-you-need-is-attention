4ad9117f864ba18530244d70da383acd75abe983
Lucas Grasso <lucasgrassoramos@gmail.com>
2026-04-26 22:56:37 -0300

    export and import model

---
+++ src/model/io.jl
@@ -0,0 +1,31 @@
+module ModelIO
+
+using JLD2
+
+function save_model(path::String, ps, st, model_config::NamedTuple)
+    jldsave(path;
+        ps           = ps,
+        st           = st,
+        model_config = model_config
+    )
+    println("Model saved to $path")
+end
+
+function load_model(path::String)
+    data = JLD2.load(path)
+    cfg  = data["model_config"]
+
+    # reconstruct model from config
+    model = AttTransformer.Transformer(
+        cfg.vocab_size,
+        cfg.d_model,
+        cfg.max_len,
+        cfg.num_layers,
+        cfg.h,
+        cfg.d_ff
+    )
+
+    return model, data["ps"], data["st"]
+end
+
+end
